6e473c8ee2f7963589249abe6d11932c96ae9d01
Peggy <64912576+pwangy@users.noreply.github.com>
2020-10-05 11:49:51 +0200

    5 passes

--- src/4-angle-calculator.js
+++ src/4-angle-calculator.js
@@ -2,10 +2,10 @@ export const angleCalculator = (turns) => {
   if (turns === 0) {
     return 0
   } else if (turns === 1) {
-      return 360 
+    return 360 
   } else if (turns === 1.5) {
-      return 540
+    return 540
   } else {
-      return 720
+    return 720
   }  
 }
