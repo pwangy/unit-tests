d291be1a6efdb9fd407061de7fa8b9b4e817a4f2
Peggy <64912576+pwangy@users.noreply.github.com>
2020-10-05 12:20:28 +0200

    6 passes

--- src/6-filter-numbers.js
+++ src/6-filter-numbers.js
@@ -1,3 +1,5 @@
-export const filterNumbers = (array, largerThan) => {
-  return array
+const numbers = [1, 20, 25, 34, 99]
+
+export const filterNumbers = (numbers) => {
+  return numbers.filter(number => number < 26)
 }
