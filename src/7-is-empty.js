3942b161700c9c7779aceb7ba1dbb4040bc586b8
Peggy <64912576+pwangy@users.noreply.github.com>
2020-10-05 15:10:27 +0200

    7 passes

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,13 @@
 export const isEmpty = (stringArrayOrObject) => {
   const type = typeof stringArrayOrObject
 
-  if (type === 'string') {
-    return stringArrayOrObject === ''
+  if ((type === 'string') && (stringArrayOrObject === '')) {
+    return true
+  } else if ((type === 'object') && (Object.keys(stringArrayOrObject).length === 0)) {
+    return true
+  } else if ((type === 'array') && (stringArrayOrObject.length >= 0)) {
+    return true
+  } else {
+    return false
   }
-
-  return false
 }
